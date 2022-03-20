e2b803fd365ed89da2fdad52be244ec7762af9d2
bd_ <bdunderscore@fushizen.net>
2022-03-20 11:06:32 -0700

    Add unitypackage export script

---
+++ Assets/bd_/avrc_dev_util/Editor/Exporter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Exporter : MonoBehaviour
+{
+    [MenuItem("bd_/AVRC Dev/Export")]
+    private static void Export()
+    {
+        AssetDatabase.ExportPackage("Assets/bd_/avrc", "avrc-dev.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
