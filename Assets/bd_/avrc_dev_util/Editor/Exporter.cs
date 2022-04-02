0c09b9c8709c5ac87b5b2527800471babe104423
bd_ <bdunderscore@fushizen.net>
2022-04-01 22:09:37 -0700

    Adding license package generation logic to exporter

--- Assets/bd_/avrc_dev_util/Editor/Exporter.cs
+++ Assets/bd_/avrc_dev_util/Editor/Exporter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,12 +12,19 @@ namespace net.fushizen.avrc
         {
             var assets = AssetDatabase.FindAssets("*", new[] {"Assets/bd_/avrc"})
                 .Where(p => !p.EndsWith("Preferences.asset"))
+                .Where(p => !p.EndsWith("License.asset"))
                 .Where(p => !p.Contains("/NoExport"))
                 .Select(AssetDatabase.GUIDToAssetPath)
                 .ToArray();
             Debug.LogWarning(string.Join(", ", assets));
-
             AssetDatabase.ExportPackage(assets, "avrc-dev.unitypackage");
+
+            var assembly = Assembly.GetAssembly(typeof(AvrcParameters));
+            var gen = assembly.GetType("net.fushizen.avrc.NoExportLicenseGen", true);
+            gen.GetMethod("GenerateLicense", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, null);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.ExportPackage("Assets/bd_/avrc/License.asset", "avrc-license.unitypackage");
         }
     }
 }
